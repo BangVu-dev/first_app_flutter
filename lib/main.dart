c76c5ebe60ea73a93f4df3246a9d11ab817eed38
BangVu-dev <bangvu16102001@gmail.com>
2022-07-25 15:28:06 +0700

    Updating Correctly with Stateful Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() => {runApp(MyApp())};
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
   void answerQuestion() {
-    print('hahahah ao ma canada!');
+    setState(() {
+      questionIndex += 1;
+    });
+    print(questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'Who is the most handsome man in the world?',
+      'What\'s your name?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Ao That Dei'),
+          title: Text('Answer question!'),
         ),
         body: Column(
           children: [
-            Text('The question!'),
+            Text(questions[questionIndex]),
             ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1')),
             ElevatedButton(onPressed: null, child: Text('Answer 1')),
             ElevatedButton(onPressed: null, child: Text('Answer 1')),
